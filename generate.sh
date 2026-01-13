80eb6e4067872ea29f92e11797821148c0ed375d
Robert Dundon <rdundon@symplr.com>
2026-01-12 22:03:49 -0500

    Add `SCRIPT_DIR` bash var so I can use script outside location and where Confluence content exports live

--- generate.sh
+++ generate.sh
@@ -3,6 +3,9 @@
 # Generate github markdown pages from confluence export
 ########################################################################
 
+# From https://stackoverflow.com/a/246128
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
 set -e
 
 echo "Creating output directories"
@@ -10,17 +13,17 @@ mkdir -pv out/page-xml
 mkdir -pv out/wiki/images
 
 echo "Generating page xmls and image mapping"
-xsltproc entities.xsl entities.xml
+xsltproc $SCRIPT_DIR/entities.xsl entities.xml
 
 echo "Copying images from attachments"
-xsltproc image-mappings.xsl out/image-mappings.xml | bash
+xsltproc $SCRIPT_DIR/image-mappings.xsl out/image-mappings.xml | bash
 
 
 echo "Convert page xmls to github markdown"
 for PAGE_PATH in out/page-xml/*.xml; do 
    PAGE_XML=${PAGE_PATH##out/page-xml/}
    PAGE_MD=${PAGE_XML%%.xml}.md
-   xsltproc --path . page.xsl "${PAGE_PATH}" > "out/wiki/${PAGE_MD}"
+   xsltproc --path . $SCRIPT_DIR/page.xsl "${PAGE_PATH}" > "out/wiki/${PAGE_MD}"
 done
 
 echo "Content generated to out/wiki"
